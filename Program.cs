91be5a617d487ce4ee86ce8622e29a024d4fec2b
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2023-06-13 01:02:59 -0700

    practice for class and object

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace practiceForcClassAndObject
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher tch1 = new Teacher();//Teacher is class name. tch1 is object name. we can take many objects from one class and every object has different properties.
+            tch1.name = "Ammar";//name is one property of class that i can give object
+            tch1.className = "programming";// class name is one property of class that i can give object
+            tch1.teacherPhoneNum = "9999999";//teacher number is one property of class that i can give object
+
+            Console.WriteLine("teacher name: " +tch1.name);
+            Console.WriteLine("class name: " + tch1.className);
+            Console.WriteLine("teacher phone number: " + tch1.teacherPhoneNum);
+
+            tch1.teacherDo();
+
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,22 @@
+﻿
+namespace practiceForcClassAndObject
+{
+    public class Teacher
+    {
+        public string name;
+        public string className;
+        public string teacherPhoneNum;
+
+
+        public void teacherDo()
+        {
+            Console.WriteLine("teacher teaching");
+            Console.WriteLine("teacher write down the attenting");
+            Console.WriteLine("teacher evaluate the student");
+
+        }
+    }
+
+   
+   
+}
